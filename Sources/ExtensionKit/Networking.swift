dd01ee904bb8c0481b5279c78562645d9ed1b31e
Muhammad Hassan <hassankhll@hotmail.com>
2022-08-26 20:37:42 +0900

    Refactor first networking API to include dependency injection for easier testing

--- Sources/ExtensionKit/Networking.swift
+++ Sources/ExtensionKit/Networking.swift
@@ -7,6 +7,19 @@
 
 import Foundation
 
+protocol NetworkSession {
+    func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void)
+}
+
+extension URLSession: NetworkSession {
+    func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
+        let task = dataTask(with: url) { data, _, error in
+            completionHandler(data, error)
+        }
+        task.resume()
+    }
+}
+
 extension ExtensionKit {
     enum NetworkError: Error {
         case unknown
@@ -16,18 +29,21 @@ extension ExtensionKit {
         
         /// Responsible for handling all networking calls
         ///  - Warning: Must create before using any public APIs
-        public class HTTPClient {
+        public class NetworkManager {
             
             public init() {}
-            private let session = URLSession.shared
+            internal var session: NetworkSession = URLSession.shared
             
+            /// Calls to the live internet to retrieve data from a given URL
+            /// - Parameters:
+            ///   - url: The location you wish to fetch data from
+            ///   - completionHandler: Returns a result object which signifies the status of the request
             public func loadData(from url: URL,
                                  completionHandler: @escaping (Result<Data, Error>) -> Void) {
-                let task = session.dataTask(with: url) { data, response, error in
+                session.get(from: url) { data, error in
                     let result = data.map(Result<Data, Error>.success) ?? .failure(error ?? NetworkError.unknown)
                     completionHandler(result)
                 }
-                task.resume()
             }
         }
     }
--- Tests/ExtensionKitTests/NetworkingTests.swift
+++ Tests/ExtensionKitTests/NetworkingTests.swift
@@ -8,21 +8,34 @@
 import XCTest
 @testable import ExtensionKit
 
+class NetworkSessionMock: NetworkSession {
+    var data: Data?
+    var error: Error?
+    
+    func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
+        completionHandler(data, error)
+    }
+}
+
 final class NetworkingTests: XCTestCase {
     
     func testLoadDataCall() {
-        let httpClient = ExtensionKit.Networking.HTTPClient()
+        let networkManager = ExtensionKit.Networking.NetworkManager()
+        let session = NetworkSessionMock()
+        networkManager.session = session
         let expectation = XCTestExpectation(description: "Load request is successful")
-        let url = URL(string: "https://www.apple.com")!
-        httpClient.loadData(from: url) { result in
+        let data = Data([0, 1, 1, 0])
+        session.data = data
+        let url = URL(fileURLWithPath: "url")
+        networkManager.loadData(from: url) { result in
             expectation.fulfill()
             switch result {
             case .success(let returnedData):
-                XCTAssertNotNil(returnedData)
+                XCTAssertEqual(data, returnedData, "HTTP Client returned unexpected data")
             case .failure(let error):
                 XCTFail(error.localizedDescription)
             }
         }
-        wait(for: [expectation], timeout: 10)
+        wait(for: [expectation], timeout: 1)
     }
 }
